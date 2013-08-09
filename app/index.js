c5a82c4ec83dc9e11562f5528a53d8b3982821df
Daniel Chatfield <chatfielddaniel@gmail.com>
2013-08-09 20:32:46 +0100

    Remove dependency install (we don't have any)

--- app/index.js
+++ app/index.js
@@ -7,10 +7,6 @@ var yeoman = require('yeoman-generator');
 var AppengineGenerator = module.exports = function AppengineGenerator(args, options, config) {
   yeoman.generators.Base.apply(this, arguments);
 
-  this.on('end', function () {
-    this.installDependencies({ skipInstall: options['skip-install'] });
-  });
-
   this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
 };
 
